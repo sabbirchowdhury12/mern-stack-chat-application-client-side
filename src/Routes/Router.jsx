604b05b7cce3d321279e2bab81acb305053babd7
sabbir <sabbir@gmail.com>
2022-12-01 18:43:26 +0600

    register page style

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,8 +1,16 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter, } from "react-router-dom";
+import Chat from "../pages/Chat";
+import Login from "../pages/Login";
 import Register from "../pages/Register";
 export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Chat />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
     {
         path: '/register',
         element: <Register />
